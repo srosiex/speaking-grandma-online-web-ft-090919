990eb897787ab5c7d514b63140190519d58315e5
Sarah H <sarahrose.henderson@gmail.com>
2019-09-09 18:04:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,9 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+if phrase != phrase.upcase
+  return "HUH! SPEAK UP, SONNY!"
+end
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
